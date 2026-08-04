321673b235b2c099f121f487274d4dc67a380c8a
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2026-08-04 23:20:22 +0545

    Revert changes

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,18 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
-  @Get()
-  async findAllTasks() {
-    return this.taskService.findAllTasks();
-  }
-
-  @Post()
-  async createTask(@Body() createTaskDto: CreateTaskDto) {
-    return this.taskService.createTask(createTaskDto);
-  }
+  // TODO: @Get()        findAllTasks()
+  // TODO: @Get(':id')   findTaskById()
+  // TODO: @Post()       createTask()
+  // TODO: @Patch(':id') updateTask()
+  // TODO: @Delete(':id') deleteTask()
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,46 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TaskService {
   constructor(private readonly prisma: PrismaService) { }
 
-  async findAllTasks() {
-    return this.prisma.task.findMany({
-      orderBy: { createdAt: 'desc' },
-    });
-  }
-
-  async findTaskById(id: number) {
-    const task = await this.prisma.task.findUnique({ where: { id } });
-    if (!task) {
-      throw new NotFoundException(`Task with id ${id} not found`);
-    }
-    return task;
-  }
-
-  async createTask(createTaskDto: CreateTaskDto) {
-    return this.prisma.task.create({
-      data: {
-        title: createTaskDto.title,
-        description: createTaskDto.description,
-        completed: createTaskDto.completed ?? false,
-      },
-    });
-  }
-
-  async updateTask(id: number, updateTaskDto: UpdateTaskDto) {
-    await this.findTaskById(id);
-    return this.prisma.task.update({
-      where: { id },
-      data: updateTaskDto,
-    });
-  }
-
-  async deleteTask(id: number) {
-    await this.findTaskById(id);
-    return this.prisma.task.delete({ where: { id } });
-  }
+  // TODO: implement findAllTasks()
+  // TODO: implement findTaskById(id)
+  // TODO: implement createTask(dto)
+  // TODO: implement updateTask(id, dto)
+  // TODO: implement deleteTask(id)
 }
